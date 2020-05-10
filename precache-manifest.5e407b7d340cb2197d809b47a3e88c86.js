cedf3178d06352c6ca40b8e9116e29ed6f407a6b
Polar stoat <polarstoat@users.noreply.github.com>
2020-05-10 18:15:56 +0100

    Updates

---
+++ precache-manifest.5e407b7d340cb2197d809b47a3e88c86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53eb813938f69efa42fcb68add7b6228",
+    "url": "/pizza-pi/index.html"
+  },
+  {
+    "revision": "840fb1d97ff640a39867",
+    "url": "/pizza-pi/static/js/2.0d9360bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pizza-pi/static/js/2.0d9360bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7456c2c6e254137ba627",
+    "url": "/pizza-pi/static/js/main.c7e8e637.chunk.js"
+  },
+  {
+    "revision": "70055d642f6acb1ed03b",
+    "url": "/pizza-pi/static/js/runtime-main.6f379e0b.js"
+  }
+]);
